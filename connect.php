3763a9581d83985b05db8edf1bcc0742e18134ec
Fabio Henrique Silva Furtado <59581131+fhsfurtado@users.noreply.github.com>
2021-03-26 15:26:09 -0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set('America/Fortaleza');
     try{
-        $bd = new PDO('mysql:host=m3t0d1st4slz.mysql.dbaas.com.br;dbname=m3t0d1st4slz;charset=utf8', "m3t0d1st4slz", "M3t0d1st4@!#");
+        $bd = new PDO('mysql:host=#;dbname=#;charset=utf8', "#", "#");
         $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
     
